3f87d494b5bd6a297003c432af1775e7a6b41561
Vedant1607 <vedantsinha1607@gmail.com>
2025-03-29 23:23:44 +0530

    Delete Button

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const buttonEl = document.getElementById("addTodo");
 
 let count = 1;
+
+function deleteTodo(index){
+    const element = document.getElementById(index);
+    element.parentElement.removeChild(element);
+}
+
 function addTodo(){
     const inputElement = document.querySelector("input");
     const inputText = inputElement.value;
+
     const divEl = document.createElement("h4");
-    divEl.innerHTML = `${count++}. ${inputText}`;
+    divEl.setAttribute("id",count);
+    divEl.innerHTML = `${count}. ${inputText} <button onclick='deleteTodo(${count})'>Delete</button>`;
+    count++;
     const parentElement = document.querySelector("body");
     parentElement.appendChild(divEl);
 }
