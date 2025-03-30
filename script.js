9143bba2f30f635f146e4433580a5fc5c604aa05
Vedant1607 <vedantsinha1607@gmail.com>
2025-03-30 20:28:30 +0530

    Updated Code

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 const buttonEl = document.getElementById("addTodo");
 
-let count = 1;
+let count = 0;
 
 function deleteTodo(index){
     const element = document.getElementById(index);
-    element.parentElement.removeChild(element);
+    // element.parentElement.removeChild(element);
+    if (element) {
+        element.remove();
+    } else {
+        console.error(`Task with ID task-${index} not found.`);
+    }
 }
 
 function addTodo(){
-    const inputElement = document.querySelector("input");
-    const inputText = inputElement.value;
+    const input = document.querySelector('input');
+    const inputText = input.value.trim();
 
-    const divEl = document.createElement("h4");
-    divEl.setAttribute("id",count);
-    divEl.innerHTML = `${inputText} <button onclick='deleteTodo(${count})'>Delete</button>`;
-    count++;
-    const parentElement = document.querySelector("body");
-    parentElement.appendChild(divEl);
+    if (inputText === ""){
+        alert("Task cannot be empty!");
+        return;
+    }
+
+    const taskContainer = document.createElement("div");
+    taskContainer.setAttribute("id",count)
+
+    const taskText = document.createElement("h4");
+    taskText.textContent = inputText;
+
+    const delBtn = document.createElement('button');
+    delBtn.textContent = 'Delete';
+    delBtn.addEventListener("click",() => deleteTodo(count));
+
+    taskContainer.appendChild(taskText);
+    taskContainer.appendChild(delBtn);
+
+    document.body.appendChild(taskContainer);
+
+    input.value = "";
 }
+count++;
 
 buttonEl.addEventListener('click',addTodo);
